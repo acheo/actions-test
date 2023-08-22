42b917c88ac5a803f1da9475d4c22b6777682d15
Adam <adam@cheong.co>
2023-08-22 13:34:55 +0100

    Move jq stuff to node

---
+++ extract.js
@@ -0,0 +1,29 @@
+const jq = require('node-jq');
+const fs = require('fs');
+const { languages } = require('./languages');
+
+async function extractFile(language) {
+    // some locales hyphens are replaced by underscores in the source json
+    let code = language;
+    if (language === 'es-419') code = 'es_419';
+    if (language === 'pt-BR') code = 'pt_BR';
+
+    const filter = ".data | map({(.attributes.key): .attributes.value_"+code+"}) | add";
+    const options = {};
+    const inFile = "./temp/translations.json";
+    const outFile = "./uploads/translation-"+language+".json";
+
+    jq.run(filter, inFile, options)
+    .then((output) => {
+        fs.writeFileSync(outFile,output);
+        console.log("saved "+outFile);
+    })
+    .catch((err) => {
+        console.error(err)
+    })
+}
+
+languages.forEach((language)=>{
+    console.log("extracting "+language);
+    extractFile(language);
+})
